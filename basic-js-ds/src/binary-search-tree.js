121ad685a6d895fa60a68903ec93702aa5b7f3ca
Dimcom1010 <dimcom1010@gmail.com>
2023-04-15 21:30:57 +0300

    add has methods, add methods,max min methods

--- basic-js-ds/src/binary-search-tree.js
+++ basic-js-ds/src/binary-search-tree.js
@@ -7,20 +7,56 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
+  tree = { parent: null, value: null, smallerChild: null, biggerChild: null }
+
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.tree.value
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (!this.tree.value) {
+      this.tree.value = data
+    } else {
+      const rec = (data, treeItem) => {
+        if (data > treeItem.value) {
+          if (!treeItem.biggerChild) {
+            treeItem.biggerChild = { parent: treeItem.value, value: data, smallerChild: null, biggerChild: null }
+          } else {
+            rec(data, treeItem.biggerChild)
+          }
+        } else {
+          if (!treeItem.smallerChild) {
+            treeItem.smallerChild = { parent: treeItem.value, value: data, smallerChild: null, biggerChild: null }
+          } else {
+            rec(data, treeItem.smallerChild)
+          }
+        }
+
+      }
+      rec(data, this.tree)
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    const rec = (treeItem) => {
+      if (treeItem.value === data) {
+        return true
+      } else if (treeItem.value < data) {
+        if (treeItem.biggerChild) {
+          rec(treeItem.biggerChild)
+        } else {
+          return false
+        }
+      } else {
+        if (treeItem.smallerChild) {
+          rec(treeItem.smallerChild)
+        } else {
+          return false
+        }
+      }
+    }
+    rec(this.tree)
   }
 
   find(/* data */) {
@@ -33,14 +69,38 @@ class BinarySearchTree {
     // remove line with error and write your code here
   }
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  max() {
+    let res=null
+    const rec = (treeItem) => {
+        if (treeItem.biggerChild) {
+            rec(treeItem.biggerChild)
+        } else {
+            res = treeItem.value
+        }
+    }
+    if (!this.tree.biggerChild) {
+        return
+    } else {
+         rec(this.tree)
+    }
+    return res
   }
 
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  min() {
+    let res=null
+    const rec = (treeItem) => {
+        if (treeItem.smallerChild) {
+            rec(treeItem.smallerChild)
+        } else {
+            res = treeItem.value
+        }
+    }
+    if (!this.tree.smallerChild) {
+        return
+    } else {
+         rec(this.tree)
+    }
+    return res
   }
 }
 
